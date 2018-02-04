ae814a7e691115c2b153b5fb1f631d05b36f51f1
Thibaut Moiroud <thib.moiroud@gmail.com>
2018-02-05 00:16:27 +0100

    Fichier de propriétées + Singleton

---
+++ src/com/ort/risk/RiskProperties.java
@@ -0,0 +1,41 @@
+package com.ort.risk;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class RiskProperties {
+	
+	private static Properties prop = new Properties();
+	
+	public static final String SAVED_MAP_PATH_PROP = "savedMapPath";
+	public static final String CURRENT_MAP_PATH_PROP = "currentMapPath";
+
+	private RiskProperties () {
+		FileInputStream input = null;
+		try {
+			input = new FileInputStream("config.properties");
+			prop.load(input);
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+	
+	public static RiskProperties getInstance() {
+		return new RiskProperties();
+	}
+	
+	public String getProperty(String key) {
+		return prop.getProperty(key);
+	}
+	
+}
