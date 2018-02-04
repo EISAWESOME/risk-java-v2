66a5686fe099697053f36cdf546f35f2d64b1163
Thibaut Moiroud <thib.moiroud@gmail.com>
2018-02-05 00:17:04 +0100

    Handler des fichiers XML des Map

---
+++ src/com/ort/risk/game/MapFileHandler.java
@@ -0,0 +1,70 @@
+package com.ort.risk.game;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.ort.risk.RiskProperties;
+
+import java.nio.file.*;
+
+public class MapFileHandler {
+
+	RiskProperties properties = RiskProperties.getInstance();
+	public static final String CURRENT_MAP_NAME = "played_map.xml";
+			
+	public void moveMapFileToCurrent(File map) {
+		try {
+			Files.copy(
+					map.toPath(),
+					Paths.get(properties.getProperty(RiskProperties.CURRENT_MAP_PATH_PROP) + CURRENT_MAP_NAME),
+					StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			System.out.println(String.format(
+					"%s.moveMapFileToCurrent() - %s",
+					getClass().getName(),
+					e.getMessage()));
+			e.printStackTrace();
+		}
+	}
+	
+	public File getCurrentMapFile() {
+		return new File(RiskProperties.CURRENT_MAP_PATH_PROP + CURRENT_MAP_NAME);
+	}
+	
+	public void saveMap(File map) {
+		try {
+			Files.copy(
+					map.toPath(),
+					Paths.get(properties.getProperty(RiskProperties.SAVED_MAP_PATH_PROP) + map.getName()),
+					StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			System.out.println(String.format(
+					"%s.saveMap() - %s",
+					getClass().getName(),
+					e.getMessage()));
+			e.printStackTrace();
+		}
+	}
+	
+	public List<File> getSavedMapFiles() {
+		List<File> savedMapFiles = new ArrayList<File>();
+		try {
+			savedMapFiles = Files.walk(Paths.get(properties.getProperty(RiskProperties.SAVED_MAP_PATH_PROP)))
+			        .filter(Files::isRegularFile)
+			        .map(Path::toFile)
+			        .collect(Collectors.toList());
+		} catch (IOException e) {
+			System.out.println(String.format(
+					"%s.getSavedMapFiles() - %s [%s]",
+					getClass().getName(),
+					e.getMessage(),
+					properties.getProperty(RiskProperties.SAVED_MAP_PATH_PROP)));
+			e.printStackTrace();
+		}
+		return savedMapFiles;
+	}
+	
+}
